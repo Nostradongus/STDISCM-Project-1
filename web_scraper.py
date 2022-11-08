b4d8bee1b39e0a4e5f226c2f0a28c70fda144bde
Nostradongus <joshue_jadie@dlsu.edu.ph>
2022-11-08 09:16:52 +0800

    added main python file

---
+++ web_scraper.py
@@ -0,0 +1,14 @@
+"""
+STDISCM Parallel Programming Project
+
+Web Scraper
+
+Group 2
+- Andre Dominic Ponce
+- Joshue Salvador Jadie 
+
+Website scraped: https://www.dlsu.edu.ph
+"""
+
+if __name__ == "__main__":
+    pass
